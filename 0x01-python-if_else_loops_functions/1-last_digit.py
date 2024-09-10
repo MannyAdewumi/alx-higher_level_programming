4c74da1511436ec3ac0fb303a9c11d3d25727fa0
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-09-10 13:15:42 -0700

    the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 lastdigit = abs(number) % 10
 if number < 0:
-    lasttdigit = -(lastdigit)
+    lastdigit = -(lastdigit)
 the_str = "Last digit of {} is {}".format(number,lastdigit)
 if lastdigit > 5:
     print(f"{the_str} and is greater than 5")
