69a11d47043bf5dc7959585a8f179cc072ab912f
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-09-11 02:02:40 -0700

    uppercase-8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for iterate in str:
         temp = iterate
         if ord(temp) >= 97 and ord(temp) <= 122:
-            temp = chr(ord(iterator) - 32)
+            temp = chr(ord(iterate) - 32)
         print("{}".format(temp), end='')
     print()
