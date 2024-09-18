ae0dd2c55d2006697f76211af45196668b50c315
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-09-18 05:43:50 -0700

    multiply by two

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 def multiply_by_2(a_dictionary):
     new_dic = {}
     if a_dictionary:
-        for key, value in a_dictionary.item():
+        for key, value in a_dictionary.items():
             new_dic.update({key: value * 2})
         return new_dic
