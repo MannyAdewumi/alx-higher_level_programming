81a218ffaf48924dfad8802ce1f5956f521da5c3
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-10-02 06:26:01 -0700

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Define a square class"""
+
+
+class Square:
+    """Representation of square class"""
+
+    def __init__(self, size=0):
+        """Instantiation with size for object initialization"""
+        self.__size = size
+
+        """Getter for the private sttr size"""
+    @property
+    def size(self):
+        """property getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter property"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Function that defines current area of square"""
+        return (self.__size * self.__size)
