5eaaa32771b7273a838f7284bc644c87fe504a5d
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-10-02 05:37:03 -0700

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     def __init__(self, size):
         """Instantiation with size for object initialization"""
 
-        if not isinstance(self, size=0):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
