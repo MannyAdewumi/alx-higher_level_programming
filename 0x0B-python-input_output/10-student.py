a72321992fd9b844deb4ca6e41efcb189e26d133
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-10-16 03:30:24 -0700

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Module 9-student.
-Creates a Student class.
-"""
+"""Creates a Student class."""
 
 
 class Student:
@@ -10,7 +8,6 @@ class Student:
         - first_name
         - last_name
         - age
-    Public method to_json().
     """
 
     def __init__(self, first_name, last_name, age):
@@ -20,10 +17,14 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Retrieves a dictionary representation
-        of a Student instance.
-        Returns: the dict representation of the instance.
-        """
+        of a Student instance."""
 
-        return self.__dict__
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
