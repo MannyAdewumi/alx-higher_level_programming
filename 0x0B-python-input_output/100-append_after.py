ff402aaf328314a12f425bc3b200b806ba05e30f
MannyAdewumigit config --global user.name MannyAdewumi <mannyadewumi@gmail.com>
2024-10-16 03:58:12 -0700

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ function that inserts a line of text to a file,
+    after each line containing a specific string"""
+
+    with open(filename, "r") as f:
+        text = f.readlines()
+
+    with open(filename, "w") as fo:
+        s = ""
+        for line in text:
+            s += line
+            if search_string in line:
+                s += new_string
+        fo.write(s)
